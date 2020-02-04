8ef045c98a2656e3c572c42c892762800ae29cfb
1gunju12345 <gunjanyadav048@gmail.com>
2020-02-04 16:40:30 +0530

    Updates

---
+++ precache-manifest.63469defcda4eeed47e8bd91b7d37f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb5cc178407a5eb524f734e2d319b54",
+    "url": "/monsters-searchPage/index.html"
+  },
+  {
+    "revision": "c1e9cc45cb7b7870fcc8",
+    "url": "/monsters-searchPage/static/css/main.edf2d2eb.chunk.css"
+  },
+  {
+    "revision": "8c87cbb896efe8ec7714",
+    "url": "/monsters-searchPage/static/js/2.9afe52c7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-searchPage/static/js/2.9afe52c7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c1e9cc45cb7b7870fcc8",
+    "url": "/monsters-searchPage/static/js/main.69737a4c.chunk.js"
+  },
+  {
+    "revision": "f3d41e366c4c542e0bf8",
+    "url": "/monsters-searchPage/static/js/runtime-main.45e10e6d.js"
+  }
+]);
